c6e7396ed68741a3451492a2f90e51595471612c
NgAOyster <125856450+NgAOyster@users.noreply.github.com>
2023-09-14 08:28:22 +0000

    Main Page design Ver.1

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="container-fluid">
+  <div>
     <div class="connecting" v-if="!connected">
       <p>登入成功 !</p>
       <p>正在连接MQTT服务器...</p>
       <p>这可能需要几秒钟时间。请稍等...</p>
     </div>
-    
     <div v-else>
       <!-- Navbar Start -->
       <nav class="navbar navbar-expand-lg navbar-light sticky-top mainNav" style="padding: 10px;">
-        <a class="navbar-brand" href="#" style="color: white;">主页</a>
-        <div class="collapse navbar-collapse">
-          <div class="navbar-nav ms-auto p-2 align-items-center">
-            <div class="mx-2">目前状态: <span :class="getStatusColorClass()">{{ getStatusText() }}</span></div>
-            
-            <div class="mx-2" v-if="Timer">已连接: {{ connectionTime }}</div>
-            <div class="mx-2" v-else>未连接服务器</div>
-          </div>
-          <div class="navbar-nav ms-auto p-2 p-lg-0">
-            <div class="dropdown">
-              <button class="btn dropdown-toggle" type="button" id="logoutDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
-                <i class="fas fa-user"></i> 欢迎您, {{ username }}
-              </button>
-              <div class="dropdown-menu" aria-labelledby="logoutDropdown">
-                <a class="dropdown-item" href="#" @click="confirmLogout()" style="color: black;">登出</a>
-              </div>
-            </div>
+  <a class="navbar-brand" href="#" style="color: white;">主页</a>
+  <!-- Hamburger Menu Icon -->
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mobileNav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="mobileNav">
+    <ul class="navbar-nav ms-auto p-8 align-items-center">
+      <li class="nav-item">
+        <div class="mx-4">目前状态: <span :class="getStatusColorClass()">{{ getStatusText() }}</span></div>
+      </li>
+      <li class="nav-item">
+        <div class="mx-4" v-if="Timer">已连接: {{ connectionTime }}</div>
+        <div class="mx-4" v-else>未连接服务器</div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+          <button class="btn dropdown-toggle" type="button" id="logoutDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+            <i class="fas fa-user"></i> 欢迎您, {{ username }}
+          </button>
+          <div class="dropdown-menu" aria-labelledby="logoutDropdown">
+            <a class="dropdown-item" href="#" @click="confirmLogout()" style="color: black;">登出</a>
           </div>
         </div>
-      </nav>
+      </li>
+    </ul>
+  </div>
+</nav>
       <!-- Navbar End -->
-      <div class="container-fluid" style="min-height: 100vh;">
+      <div class="container-fluid" style="height: 100vh;">
         <div class="row">
           <div class="col-md-2 sideComp text-start">
             <!-- Sidebar Component -->
@@ -46,12 +52,10 @@
     </div>
   </div>
 </template>
-
 <script>
 import Paho from "paho-mqtt";
 import Sidebar from "../components/Sidebar.vue";
 import Content from "../components/Content.vue";
-
 export default {
   props: {
     username: String,
@@ -75,13 +79,11 @@ export default {
       selectedDataType: "temperature", // Default data type
     };
   },
-
   created() {
     this.initMQTT();
     window.addEventListener('online', this.handleOnline);
     window.addEventListener('offline', this.handleOffline);
   },
-
   methods: {
     initMQTT() {
       this.disconnect = false;
@@ -90,7 +92,6 @@ export default {
       const client = new Paho.Client(
         "ws://222.222.119.72:8083/mqtt", uniqueClientId
       );
-
       client.onConnectionLost = (responseObject) => {
         console.log("Connection lost: " + responseObject.errorMessage);
         this.connectStatus = false;
@@ -102,11 +103,9 @@ export default {
           this.reconnect();
         }, 3000);
       };
-
       client.onMessageArrived = (message) => {
         console.log("Message Received: " + message.payloadString);
         const topic = message.destinationName;
-
         if (topic.endsWith("/othertemperature")) {
           this.handleMessage(message.payloadString, topic, this.temperatureData);
         } else if (topic.endsWith("/othercurrent")) {
@@ -115,13 +114,10 @@ export default {
           // Handle other topics or do nothing
         }
       };
-
       const dgmgValues = ["dgmg01", "dgmg02", "dgmg03", "dgmg04"];
       const secondValues = ["asphaltmix", "asphaltcrush", "warmingmix", "stonecrush", "peripheral"];
       const deviceValues = Array.from({ length: 100 }, (_, index) => `device${index + 1}`);
-
       const topics = [];
-
       dgmgValues.forEach(dgmg => {
         secondValues.forEach(second => {
           deviceValues.forEach(device => {
@@ -131,7 +127,6 @@ export default {
           });
         });
       });
-
       const options = {
         useSSL: false,
         userName: this.username,
@@ -142,7 +137,6 @@ export default {
           this.connectStatus = true;
           this.reconnectStatus = false;
           this.startConnectionTimer();
-
           topics.forEach(topic => {
             client.subscribe(topic, { qos: 0 });
           });
@@ -155,7 +149,6 @@ export default {
           this.reconnect();
         },
       };
-
       client.connect(options);
     },
     reconnect() {
@@ -168,35 +161,29 @@ export default {
     handleMessage(message, topic, dataCollection) {
     const isTemperature = topic.endsWith("/othertemperature");
     const isCurrent = topic.endsWith("/othercurrent");
-
     if (isTemperature || isCurrent) {
       const jsonData = JSON.parse(message);
       const localTimeString = new Date().toLocaleString();
-
       let dgmgMessage = "普通消息";
       const topicPrefix = topic.substring(0, 6); // Get the first 6 characters of the topic
-      if (topicPrefix === "dgmg02") { dgmgMessage = "控制消息"; } 
-      else if (topicPrefix === "dgmg03") { dgmgMessage = "告警消息"; } 
+      if (topicPrefix === "dgmg02") { dgmgMessage = "控制消息"; }
+      else if (topicPrefix === "dgmg03") { dgmgMessage = "告警消息"; }
       else if (topicPrefix === "dgmg04") { dgmgMessage = "严重警告"; }
-
       let equipment = "沥青搅拌站";
       const charactersAfterSeventh = topic.substring(7, topic.indexOf('/', 7));
-      if (charactersAfterSeventh === "asphaltcrush") { equipment = "沥青料破碎"; } 
-      else if (charactersAfterSeventh === "warmingmix") { equipment = "温拌发泡设备"; } 
-      else if (charactersAfterSeventh === "stonecrush") { equipment = "骨料整形破碎"; } 
+      if (charactersAfterSeventh === "asphaltcrush") { equipment = "沥青料破碎"; }
+      else if (charactersAfterSeventh === "warmingmix") { equipment = "温拌发泡设备"; }
+      else if (charactersAfterSeventh === "stonecrush") { equipment = "骨料整形破碎"; }
       else if (charactersAfterSeventh === "peripheral") { equipment = "周边设备"; }
-
       // Extract machineId from the topic
       const topicParts = topic.split('/');
       const machineIdPart = topicParts[topicParts.length - 2]; // Assuming machineId is the second-to-last part of the topic
       const machineId = parseInt(machineIdPart.replace('device', '')); // Remove 'device' prefix and parse as an integer
-
       const data = {
         time: localTimeString,
         machineId,
         equipment,
       };
-
       if (isTemperature) {
         data.TempdgmgMessage = dgmgMessage;
         data.temp1 = jsonData.temp1;
@@ -210,13 +197,11 @@ export default {
         data.current3 = jsonData.current3;
         data.current4 = jsonData.current4;
       }
-
       const existingIndex = dataCollection.findIndex(
         (dataItem) =>
           dataItem.machineId === machineId &&
           dataItem.equipment === equipment
       );
-
       if (existingIndex !== -1) {
         dataCollection[existingIndex] = data;
       } else {
@@ -271,15 +256,15 @@ export default {
       this.selectedDataType = dataType;
     },
     getStatusColorClass() {
-      if (this.connectStatus) { return 'connected'; } 
+      if (this.connectStatus) { return 'connected'; }
       if (!this.onlineStatus) { return 'internetDisconnect'; }
-      if (this.reconnectStatus) { return 'reconnecting'; } 
+      if (this.reconnectStatus) { return 'reconnecting'; }
       if (this.disconnect) { return 'disconnected'; }
     },
     getStatusText() {
       if (!this.onlineStatus) { return '网络断开'; }
-      if (this.connectStatus) { return '已连接'; } 
-      if (this.reconnectStatus) { return '重新连接中...'; } 
+      if (this.connectStatus) { return '已连接'; }
+      if (this.reconnectStatus) { return '重新连接中...'; }
       if (this.disconnect) { return '连接断开'; }
     },
     confirmLogout() {
@@ -292,7 +277,6 @@ export default {
   },
 };
 </script>
-
 <style>
 @import '../CSS/MainPage.css';
 </style>
